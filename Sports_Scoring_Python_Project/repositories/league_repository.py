11ebcce9b61feb084b7c94197c15d7554c3688d2
Codeclan Student <codeclanstudent@Codeclans-MacBook-Pro.local>
2023-04-17 13:26:46 +0100

    save function in league repository

--- Sports_Scoring_Python_Project/repositories/league_repository.py
+++ Sports_Scoring_Python_Project/repositories/league_repository.py
@@ -2,3 +2,10 @@ from db.run_sql import run_sql
 from models.league import League
 from models.team import Team
 
+def save(league):
+    sql = "INSERT INTO league (team_name, position) VALUES (%s,%s) RETURNING *"
+    values = (league.team_name, league.position)
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    league.id = id
+    return league
